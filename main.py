885b1f867e9315487bc9242603273cdaaec1cf35
Missael Cortes <60858064+missaelcorm@users.noreply.github.com>
2022-10-16 16:13:02 -0500

    Implementing Modules
    Implementing truthTables and sets to main. Adding a flag detector for each option that the program provides

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
+import Modules.truthTables as MT
+import Modules.sets as MS
 import sys
 
 #Main() function where all magic happen
 def main():
+    ARGS = sys.argv
 
+    if ARGS[1] == '-t':
+        MT.truthtable(ARGS[2])
+    elif ARGS[1] == '-s':
+        MS.sets(ARGS)
     else:
+        MT.printHelp()
 
 #Simple try and except, if fails prints the help info 
 #about usage and syntax using printHelp(), if not 
 #main() worked
 
+"""
+"""
 try:
     main()
 except:
+    MT.printHelp()
+"""
+"""
+
+#main()
