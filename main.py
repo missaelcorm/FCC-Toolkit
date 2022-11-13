2e82988b99bf5d3e52959b1c71fd692a64d5e01a
Missael Cortes <60858064+missaelcorm@users.noreply.github.com>
2022-11-12 22:16:36 -0600

    Updating Help Menu

--- main.py
+++ main.py
@@ -4,22 +4,39 @@ import Modules.successions as MK
 import sys
 
 def printHelp():
-    MT.cleanScreen()
-    print('---------------------- Tablas de Verdad ------------------------ ')
-    print('Uso: python main.py -t "[expresion]" ')
-    print('Use unicamente estas variables: -  [p, q, r, s, t, u, w, x, y, z]')
-    print('Conjuncion: ------------------------------------------------- "^"')
-    print('Disyuncion: ------------------------------------------------- "v"')
-    print('Negacion: --------------------------------------------------- "~"')
-    print('Implicacion: ------------------------------------------------ ">"')
-    print('Doble Implicacion: ------------------------------------------ "-"')
-    print('-----------------------------------------------------------------')
-    print('Nota: Es necesario escribir la expresion entre comillas ("")')
-    print('Ejemplo: python main.py "p^qvr"')
-    print('------------------ Operaciones de Conjuntos -------------------- ')
-    print('Uso: python main.py -s [-A, -B or -C] [values]')
-    print('Declara un conjunto -A, -B o -C, y a su derecha los valores')
-    print('Ejemplo: python main.py -s -A 1,2,3 -C 2,3,4 -B 3,4,5')
+    cleanScreen()
+    print('''Uso: python main.py [Modulo] [Opciones]>
+TABLAS DE VERDAD:
+    Use unicamente estas variables: [p, q, r, s, t, u, w, x, y, z]
+    -t <"expresion">: tablas de verdad
+        ^: Conjuncion
+        v: Disyuncion
+        ~: Negacion
+        >: Implicacion
+        -: Doble Implicacion
+    Nota: Es necesario escribir la expresion entre comillas ("")
+    Ejemplo: python main.py -t "p^qvr"
+CONJUNTOS:
+    Declara un conjunto -A, -B o -C, y a su derecha los valores
+    -s: Operaciones de Conjuntos
+    -A <VALORES>: Declara un conjunto A y sus valores separados por comas
+    -B <VALORES>: Declara un conjunto B y sus valores separados por comas
+    -C <VALORES>: Declara un conjunto C y sus valores separados por comas
+    Ejemplo: python main.py -s -A 1,2,3 -C 2,3,4 -B 3,4,5
+SUCESIONES:
+    Ingresa una sucesion usando "k" como variable.
+    -k: Sucesiones
+    --expression <"expression">: La expresion a calcular, usando "k" como variable.
+        +: Suma
+        -: Resta
+        /: Division
+        *: Multiplicacion
+        ^: Potencia
+    --limInf <numero>: Limite Inferior
+    --limSup <numero>: Limite Superior
+    Opcionales:
+    --decimals <numero de decimales>: Ingresa el numero de decimales deseado (2 por default).
+    Ejemplo: python main.py -k --expression "1/k^2" --limInf 10 --limSup 30 --decimals 6''')
 
 #Main() function where all magic happen
 def main():
